fb7360ef2cb27145422fc236d0e245da6488e0b7
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-07 18:28:36 +0600

    map added with latitude and longitude

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { APIProvider } from "@vis.gl/react-google-maps";
+import { googleMapAPIKey } from "./map-api-key.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <APIProvider apiKey={googleMapAPIKey}>
+      <App />
+    </APIProvider>
+  </StrictMode>
+);
